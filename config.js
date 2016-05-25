95cc906054cfd5d4a2b330f5ba0c5459aa57d955
Christian Stussak <stussak@mfo.de>
2016-05-25 14:55:17 +0200

    Add slider via Swiper

---
+++ config.js
@@ -0,0 +1,20 @@
+var config = [{
+        "websiteUrl": "http://imaginary.org/snapshot/polyhedra-and-commensurability",
+        "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/2016-009-guglielmetti-jacquemet.pdf",
+    }, {
+        "websiteUrl": "http://imaginary.org/snapshot/the-mystery-of-sleeping-sickness-why-does-it-keep-waking-up",
+        "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/snapshot-2015-015.pdf",
+    }, {
+        "websiteUrl": "http://imaginary.org/snapshot/on-the-containment-problem",
+        "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/2016-003-szemberg-szpond.pdf",
+    }, {
+        "websiteUrl": "http://imaginary.org/snapshot/random-sampling-of-domino-and-lozenge-tilings",
+        "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/2016-002-fusy.pdf",
+    },
+    /* template
+    {
+        "websiteUrl": "",
+        "pdfUrl": "",
+    },
+    */
+];
