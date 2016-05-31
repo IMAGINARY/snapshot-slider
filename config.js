4faba20f4b2d07f14808084001e9865f07c8fb40
Christian Stussak <stussak@mfo.de>
2016-05-31 10:13:31 +0200

    Use shortened URL for QR code

--- config.js
+++ config.js
@@ -1,19 +1,24 @@
 var config = [{
         "websiteUrl": "http://imaginary.org/snapshot/polyhedra-and-commensurability",
+        "shortWebsiteUrl": "http://bit.ly/20R3gLI",
         "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/2016-009-guglielmetti-jacquemet.pdf",
     }, {
         "websiteUrl": "http://imaginary.org/snapshot/the-mystery-of-sleeping-sickness-why-does-it-keep-waking-up",
+        "shortWebsiteUrl": "http://bit.ly/1Z9M5Ey",
         "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/snapshot-2015-015.pdf",
     }, {
         "websiteUrl": "http://imaginary.org/snapshot/on-the-containment-problem",
+        "shortWebsiteUrl": "http://bit.ly/22vcTBa",
         "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/2016-003-szemberg-szpond.pdf",
     }, {
         "websiteUrl": "http://imaginary.org/snapshot/random-sampling-of-domino-and-lozenge-tilings",
+        "shortWebsiteUrl": "http://bit.ly/27ZY6SZ",
         "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/2016-002-fusy.pdf",
     },
     /* template
     {
         "websiteUrl": "",
+        "shortWebsiteUrl": "",
         "pdfUrl": "",
     },
     */
