818223c8cbcfcdf66dba4eb707106d416c477071
Christian Stussak <stussak@mfo.de>
2016-06-06 14:44:37 +0200

    Add config for all snapshots available to date

--- config.js
+++ config.js
@@ -1,19 +1,214 @@
+// list snapshots ordered by snapshot id (most recent first)
 var config = [{
-        "websiteUrl": "http://imaginary.org/snapshot/polyhedra-and-commensurability",
-        "shortWebsiteUrl": "http://bit.ly/20R3gLI",
-        "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/2016-009-guglielmetti-jacquemet.pdf",
+        author: 'Rafael Guglielmetti, Matthieu Jacquemet ',
+        title: 'Polyhedra and commensurability',
+        websiteUrl: 'https://imaginary.org/snapshot/polyhedra-and-commensurability',
+        shortWebsiteUrl: 'http://bit.ly/1tbCC5P',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2016-009-guglielmetti-jacquemet.pdf'
     }, {
-        "websiteUrl": "http://imaginary.org/snapshot/the-mystery-of-sleeping-sickness-why-does-it-keep-waking-up",
-        "shortWebsiteUrl": "http://bit.ly/1Z9M5Ey",
-        "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/snapshot-2015-015.pdf",
+        author: 'Manfred Deistler, Andreas Graef',
+        title: 'Fokus-Erkennung bei Epilepsiepatienten mithilfe moderner Verfahren der Zeitreihenanalyse',
+        websiteUrl: 'https://imaginary.org/snapshot/fokus-erkennung-bei-epilepsiepatienten-mithilfe-moderner-verfahren-der-zeitreihenanalyse',
+        shortWebsiteUrl: 'http://bit.ly/1XwrNbn',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2016-008-deistler-graef.pdf'
     }, {
-        "websiteUrl": "http://imaginary.org/snapshot/on-the-containment-problem",
-        "shortWebsiteUrl": "http://bit.ly/22vcTBa",
-        "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/2016-003-szemberg-szpond.pdf",
+        author: 'Anthony T. Patera, Karsten Urban',
+        title: 'High performance computing on smartphones',
+        websiteUrl: 'https://imaginary.org/snapshot/high-performance-computing-on-smartphones',
+        shortWebsiteUrl: 'http://bit.ly/1XwsAZP',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2016-006-urban-patera.pdf'
     }, {
-        "websiteUrl": "http://imaginary.org/snapshot/random-sampling-of-domino-and-lozenge-tilings",
-        "shortWebsiteUrl": "http://bit.ly/27ZY6SZ",
-        "pdfUrl": "http://imaginary.org/sites/default/files/snapshots/2016-002-fusy.pdf",
+        author: 'Eugenio Giannelli, Jay Taylor ',
+        title: 'Symmetry and characters of finite groups',
+        websiteUrl: 'https://imaginary.org/snapshot/symmetry-and-characters-of-finite-groups',
+        shortWebsiteUrl: 'http://bit.ly/1XwrRrF',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2016-005-gianelli-taylor.pdf'
+    }, {
+        author: 'Maria Dostert, Stefan Krupp, Jan Hendrik Rolfes',
+        title: 'Das Problem der Kugelpackung',
+        websiteUrl: 'https://imaginary.org/snapshot/das-problem-der-kugelpackung',
+        shortWebsiteUrl: 'http://bit.ly/1XwsiSA',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2016-004-dostert-rolfes-krupp.pdf'
+    }, {
+        author: 'Tomasz Szemberg, Justyna Szpond',
+        title: 'On the containment problem',
+        websiteUrl: 'https://imaginary.org/snapshot/on-the-containment-problem',
+        shortWebsiteUrl: 'http://bit.ly/1XwshxW',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2016-003-szemberg-szpond.pdf'
+    }, {
+        author: 'Éric Fusy ',
+        title: 'Random sampling of domino and lozenge tilings',
+        websiteUrl: 'https://imaginary.org/snapshot/random-sampling-of-domino-and-lozenge-tilings',
+        shortWebsiteUrl: 'http://bit.ly/1XwrT2A',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2016-002-fusy.pdf'
+    }, {
+        author: 'Magnus Egerstedt',
+        title: 'Swarming robots',
+        websiteUrl: 'https://imaginary.org/snapshot/swarming-robots',
+        shortWebsiteUrl: 'http://bit.ly/1XwsxNB',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2016-001.pdf'
+    }, {
+        author: 'Ulrich Krähmer',
+        title: 'From computer algorithms to quantum field theory: an introduction to operads',
+        websiteUrl: 'https://imaginary.org/snapshot/from-computer-algorithms-to-quantum-field-theory-an-introduction-to-operads',
+        shortWebsiteUrl: 'http://bit.ly/1XwrYDy',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-017.pdf'
+    }, {
+        author: 'Juanjo Rué ',
+        title: 'Domino tilings of the Aztec Diamond',
+        websiteUrl: 'https://imaginary.org/snapshot/domino-tilings-of-the-aztec-diamond',
+        shortWebsiteUrl: 'http://bit.ly/1XwsjWE',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-016.pdf'
+    }, {
+        author: 'Sebastian Funk',
+        title: 'The mystery of sleeping sickness – why does it keep waking up?',
+        websiteUrl: 'https://imaginary.org/snapshot/the-mystery-of-sleeping-sickness-why-does-it-keep-waking-up',
+        shortWebsiteUrl: 'http://bit.ly/1XwrPQp',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-015.pdf'
+    }, {
+        author: 'Antti Knowles',
+        title: 'Quantum diffusion',
+        websiteUrl: 'https://imaginary.org/snapshot/quantum-diffusion',
+        shortWebsiteUrl: 'http://bit.ly/1Xws262',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2015-014-knowles.pdf'
+    }, {
+        author: 'Amanda Swan, Albert Murtha',
+        title: 'Modelling the spread of brain tumours',
+        websiteUrl: 'https://imaginary.org/snapshot/modelling-the-spread-of-brain-tumours',
+        shortWebsiteUrl: 'http://bit.ly/1XwrQnz',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2015-013-swan-murtha.pdf'
+    }, {
+        author: 'J. S. Marron',
+        title: 'Visual Analysis of Spanish Male Mortality',
+        websiteUrl: 'https://imaginary.org/snapshot/visual-analysis-of-spanish-male-mortality',
+        shortWebsiteUrl: 'http://bit.ly/1XwsCkp',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2015-012-marron.pdf'
+    }, {
+        author: 'Christopher J. Sangwin',
+        title: 'Curriculum development in university mathematics: where mathematicians and education collide',
+        websiteUrl: 'https://imaginary.org/snapshot/curriculum-development-in-university-mathematics-where-mathematicians-and-education-collide',
+        shortWebsiteUrl: 'http://bit.ly/1XwrX2v',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2015-011-sangwin.pdf'
+    }, {
+        author: 'Jürg Kramer, Anna-Maria von Pippich',
+        title: 'Special values of zeta functions and areas of triangles',
+        websiteUrl: 'https://imaginary.org/snapshot/special-values-of-zeta-functions-and-areas-of-triangles',
+        shortWebsiteUrl: 'http://bit.ly/1XwrJbn',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2015-010-pippich-kramer.pdf'
+    }, {
+        author: 'Victoria Powers',
+        title: 'How to choose a winner: the mathematics of social choice',
+        websiteUrl: 'https://imaginary.org/snapshot/how-to-choose-a-winner-the-mathematics-of-social-choice',
+        shortWebsiteUrl: 'http://bit.ly/1Xwsots',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/2015-009-powers.pdf'
+    }, {
+        author: 'Valentina Kiritchenko, Evgeny Smirnov, Vladlen Timorin',
+        title: 'Ideas of Newton-Okounkov bodies',
+        websiteUrl: 'https://imaginary.org/snapshot/ideas-of-newton-okounkov-bodies',
+        shortWebsiteUrl: 'http://bit.ly/1XwrWvt',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-008.pdf'
+    }, {
+        author: 'Ben Schweizer',
+        title: 'Darcy\'s law and groundwater flow modelling ',
+        websiteUrl: 'https://imaginary.org/snapshot/darcys-law-and-groundwater-flow-modelling',
+        shortWebsiteUrl: 'http://bit.ly/1XwrP2N',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-007.pdf'
+    }, {
+        author: 'Raluca Eftimie',
+        title: 'Modeling communication and movement: from cells to animals and humans',
+        websiteUrl: 'https://imaginary.org/snapshot/modelling-communication-and-movement-from-cells-to-animals-and-humans',
+        shortWebsiteUrl: 'http://bit.ly/1XwrPQl',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/communication_1.pdf'
+    }, {
+        author: 'Tom Solomon',
+        title: 'Chaos and chaotic fluid mixing',
+        websiteUrl: 'https://imaginary.org/snapshot/chaos-and-chaotic-fluid-mixing',
+        shortWebsiteUrl: 'http://bit.ly/1XwrYmU',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-005.pdf'
+    }, {
+        author: 'Thorsten Holm',
+        title: 'Friezes and tilings',
+        websiteUrl: 'https://imaginary.org/snapshot/friezes-and-tilings',
+        shortWebsiteUrl: 'http://bit.ly/1XwrSvz',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-004.pdf'
+    }, {
+        author: 'George Willis',
+        title: 'Zero-dimensional symmetry',
+        websiteUrl: 'https://imaginary.org/snapshot/zero-dimensional-symmetry',
+        shortWebsiteUrl: 'http://bit.ly/1XwskKc',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-003.pdf'
+    }, {
+        author: 'Christine Breiner',
+        title: 'Minimizing energy',
+        websiteUrl: 'https://imaginary.org/snapshot/minimizing-energy',
+        shortWebsiteUrl: 'http://bit.ly/1XwrYDq',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-002.pdf'
+    }, {
+        author: 'Diana Davis',
+        title: 'Billiards and flat surfaces',
+        websiteUrl: 'https://imaginary.org/snapshot/billiards-and-flat-surfaces',
+        shortWebsiteUrl: 'http://bit.ly/1XwszFd',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2015-001_0.pdf'
+    }, {
+        author: 'Peter Benner, Hermann Mena, René Schneider',
+        title: 'Drugs, herbicides, and numerical simulation',
+        websiteUrl: 'https://imaginary.org/snapshot/drugs-herbicides-and-numerical-simulation',
+        shortWebsiteUrl: 'http://bit.ly/1XwsD7X',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-010.pdf'
+    }, {
+        author: 'Greg Knese',
+        title: 'Operator theory and the singular value decomposition',
+        websiteUrl: 'https://imaginary.org/snapshot/operator-theory-and-the-singular-value-decomposition',
+        shortWebsiteUrl: 'http://bit.ly/1XwrKMt',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-009.pdf'
+    }, {
+        author: 'Alain Valette',
+        title: 'The Kadison-Singer problem',
+        websiteUrl: 'https://imaginary.org/snapshot/the-kadison-singer-problem',
+        shortWebsiteUrl: 'http://bit.ly/1XwrQnn',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-008.pdf'
+    }, {
+        author: 'Ragnar-Olaf Buchweitz, Eleonore Faber',
+        title: 'Swallowtail on the shore',
+        websiteUrl: 'https://imaginary.org/snapshot/swallowtail-on-the-shore',
+        shortWebsiteUrl: 'http://bit.ly/1Xwsdyr',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-007.pdf'
+    }, {
+        author: 'Howell Tong',
+        title: 'Statistics and dynamical phenomena',
+        websiteUrl: 'https://imaginary.org/snapshot/statistics-and-dynamical-phenomena',
+        shortWebsiteUrl: 'http://bit.ly/1Xws2mz',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-006.pdf'
+    }, {
+        author: 'Brian Harbourne, Tomasz Szemberg',
+        title: 'Arrangements of lines',
+        websiteUrl: 'https://imaginary.org/snapshot/arrangements-of-lines',
+        shortWebsiteUrl: 'http://bit.ly/1XwrZas',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-005.pdf'
+    }, {
+        author: 'Bruce Reznick',
+        title: 'What does > really mean?',
+        websiteUrl: 'https://imaginary.org/snapshot/what-does-really-mean',
+        shortWebsiteUrl: 'http://bit.ly/1XwrX2k',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-004_0.pdf'
+    }, {
+        author: 'Harald Helfgott',
+        title: 'The ternary Goldbach problem',
+        websiteUrl: 'https://imaginary.org/snapshot/the-ternary-goldbach-problem',
+        shortWebsiteUrl: 'http://bit.ly/1XwrZY8',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-003.pdf'
+    }, {
+        author: 'Wolfgang Lerche',
+        title: 'Matrixfaktorisierungen',
+        websiteUrl: 'https://imaginary.org/snapshot/matrixfaktorisierungen',
+        shortWebsiteUrl: 'http://bit.ly/1Xwsnpm',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-002.pdf'
+    }, {
+        author: 'John E. McCarthy',
+        title: 'Dirichlet Series',
+        websiteUrl: 'https://imaginary.org/snapshot/dirichlet-series',
+        shortWebsiteUrl: 'http://bit.ly/1XwrtJA',
+        pdfUrl: 'https://imaginary.org/sites/default/files/snapshots/snapshot-2014-001.pdf'
     },
     /* template
     {
