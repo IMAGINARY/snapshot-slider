2a6311c67d9f3f811dbdc78f86f49fd2a8ff33c8
Christian Stussak <stussak@mfo.de>
2016-06-16 11:50:00 +0200

    Enable basic fullscreen kiosk mode without menu bar and developer tools

--- main.js
+++ main.js
@@ -15,14 +15,19 @@ function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        kiosk: true,
+        fullscreen: true
     });
+
+    // disable menu bar
+    mainWindow.setMenu(null);
 
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
