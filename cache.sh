ea05bff88250fd3c460f6d9d37af3a7d393e46ad
Christian Stussak <stussak@mfo.de>
2016-12-09 12:00:32 +0100

    Migrate PDF download from shell+wget to nodejs+request

--- cache.sh
+++ cache.sh
@@ -3,12 +3,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 DIRNAME="content"
 BASENAME=$(basename -s '.pdf' $1)
 mkdir -p "$DIRNAME"
-if [ ! -f "$DIRNAME/$BASENAME.pdf" ]; then
-    >&2 echo "Downloading $1 to $DIRNAME/$BASENAME.pdf"
-    wget -O "$DIRNAME/$BASENAME.pdf" "$1"
-else
-    >&2 echo "$DIRNAME/$BASENAME.pdf already cached"
-fi
 if [ ! -f "$DIRNAME/$BASENAME-booklet.pdf" ]; then
     >&2 echo "Creating $DIRNAME/$BASENAME-booklet.pdf"
     EMPTY_PAGE_PDF="resources/A5-empty.pdf"
