b59313cd075507d2b3c872cd9ad9db265606193c
Christian Stussak <stussak@mfo.de>
2018-07-05 16:12:04 +0200

    Fix SnapshotListUpdater not throwing correct exceptions

--- src/js/renderer/SnapshotListUpdater.js
+++ src/js/renderer/SnapshotListUpdater.js
@@ -11,7 +11,7 @@ class UpdateError extends Error {
 
         // Maintains proper stack trace for where our error was thrown (only available on V8)
         if (Error.captureStackTrace) {
-            Error.captureStackTrace(this, SnapshotListUpdater.UpdateError);
+            Error.captureStackTrace(this, UpdateError);
         }
 
         // Custom debugging information
